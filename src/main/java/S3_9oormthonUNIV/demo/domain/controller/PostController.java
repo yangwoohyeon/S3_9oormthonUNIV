08db1a649f0f9e49122385d1ef4b45e530e3396d
yangwoohyeon <dngus0717@gmail.com>
2025-04-14 22:50:56 +0900

    객체 삭제 구현

--- src/main/java/S3_9oormthonUNIV/demo/domain/controller/PostController.java
+++ src/main/java/S3_9oormthonUNIV/demo/domain/controller/PostController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -25,5 +22,11 @@ public class PostController {
                                              @RequestParam("image") MultipartFile image) {
         postService.savePost(title, content, image);
         return ResponseEntity.ok("게시글이 성공적으로 저장되었습니다.");
+    }
+
+    @DeleteMapping("/image")
+    public ResponseEntity<String> deleteImage(@RequestParam("imageUrl") String imageUrl) {
+        postService.deleteImageFromS3(imageUrl);
+        return ResponseEntity.ok("이미지가 성공적으로 삭제되었습니다.");
     }
 }
